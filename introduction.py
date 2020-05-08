7a0423c649366bf2d02cd3e3bf43225577747b4c
apolot99 <44310628+apolot99@users.noreply.github.com>
2020-05-08 14:39:22 -0400

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -6,3 +6,24 @@
 
 #      CHAPTER 1
 
+#  The command line: two command line shells on windows i.e command shell and power shell. USE POWER SHELL IT IS STRONGER.
+
+#  Most frequently commands used in django development; 
+#  cd (change down a directory)
+#  cd..(change up a directory)
+#  ls (list files in your current directory)
+#  pwd (print working directory)
+#  mkdir (make directory)
+#  touch (create a new file)
+
+# The $ sign is our command line prompt
+
+# Virtual environments are an isolated container containing all the software dependencies of a given project.
+
+# Use a dedicated virtual environment for each new python project. i.e Pipenv
+
+# Pipenv creates a Pipfile containing software dependencies and a Pipfile.loc for ensuring deterministic builds.
+
+# Determinism means that each time and everytime you download the software in a new virtual environment, you will have exactly the same configuration.
+
+# 
