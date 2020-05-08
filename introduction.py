acdd7a114accfb01bfb15ac97d12adc761c6599f
apolot99 <44310628+apolot99@users.noreply.github.com>
2020-05-08 13:35:48 -0400

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -1,2 +1,8 @@
 # this is the introduction chapter
 
+# brevity means exact use of words in writing
+
+# django is a python-based free and open source web framework. (Just like how there is streamlit, so there is also python)
+
+#      CHAPTER 1
+
